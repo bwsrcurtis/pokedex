2c616f0b0c7bbd1e283e6e1e56de92d595aeadfe
bwsrcurtis <bwsrcurtis@gmail.com>
2023-01-16 01:01:02 -0500

    Imported pokedata json into modify_pokedata.js

--- modify_pokedata.js
+++ modify_pokedata.js
@@ -1,13 +1,10 @@
+import pokedata from './pokedata.json' assert { type: 'json' };
 
-import {pokedata} from "./pokemon_data.js";
-import { pokedexDescriptions } from "./pokedexDescriptions.js";
-import * as fs from 'fs';
-let updatedPokeData = pokedata.data.gen3_species
-let descriptions = pokedexDescriptions.data.pokemon_v2_pokemonspeciesflavortext
 
-updatedPokeData.forEach(function (x) {
-    x.imgURL = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${x.id}.png`
-    x.pokedexDescription = descriptions[x.id-1].flavor_text.replace(/[\r\n\x0B\x0C\u0085\u2028\u2029]+/g, " ")
-})
+console.log(pokedata[0])
+// updatedPokeData.forEach(function (x) {
+//     x.imgURL = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${x.id}.png`
+//     x.pokedexDescription = descriptions[x.id-1].flavor_text.replace(/[\r\n\x0B\x0C\u0085\u2028\u2029]+/g, " ")
+// })
 
-fs.writeFileSync("./pokedata.json", JSON.stringify(updatedPokeData, null), 'utf-8')
+// fs.writeFileSync("./pokedata.json", JSON.stringify(updatedPokeData, null), 'utf-8')
