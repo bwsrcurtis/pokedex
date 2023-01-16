507227fd9e1ceaed297a561c8d8d6b28f1bb33f8
bwsrcurtis <bwsrcurtis@gmail.com>
2023-01-16 14:29:15 -0500

    Cleaned up extra files

--- modify_pokedata.js
+++ modify_pokedata.js
@@ -1,28 +1,12 @@
 import pokedata from './pokedata.json' assert { type: 'json' };
-import { pokeTypes } from './pokemonTypes.js';
-import * as fs from 'fs';
-
-let types = pokeTypes
+// import * as fs from 'fs';
 let updatedPokeData = pokedata
 
-// console.log(types.pokemon_v2_pokemonsprites[0].pokemon_v2_pokemon.pokemon_v2_pokemontypes[0]['pokemon_id'])
 
-updatedPokeData.map((x) => {
-    if (x.id == types.pokemon_v2_pokemonsprites[x.id-1].pokemon_v2_pokemon.pokemon_v2_pokemontypes[0]['pokemon_id']) {
-    x.pokemon_v2_pokemon.type = [types.pokemon_v2_pokemonsprites[x.id - 1].pokemon_v2_pokemon.pokemon_v2_pokemontypes]
-    }
+updatedPokeData = updatedPokeData.map((x) => {
+    x.base_experience = x.pokemon_v2_pokemon.base_experience
 })
-// console.log([types.pokemon_v2_pokemonsprites[0].pokemon_v2_pokemon.pokemon_v2_pokemontypes].length)
-
-// console.log(pokedata[0])
-// updatedPokeData.forEach(function (x) {
-//     console.log(x.id)
-//     console.log(types.pokemon_v2_pokemonsprites[x.id - 1].pokemon_v2_pokemon.pokemon_v2_pokemontypes[0]['pokemon_id'])
-//     if (x.id == types.pokemon_v2_pokemonsprites[x.id - 1].pokemon_v2_pokemon.pokemon_v2_pokemontypes[0]['pokemon_id']) {
-//         x.pokemon_v2_pokemon.type = [types.pokemon_v2_pokemonsprites[x.id - 1].pokemon_v2_pokemon.pokemon_v2_pokemontypes[0].pokemon_v2_type]
-//     }
-// })
 
 console.log(updatedPokeData[0])
 
-fs.writeFileSync("./pokedata.json", JSON.stringify(updatedPokeData, null), 'utf-8')
+// fs.writeFileSync("./pokedata.json", JSON.stringify(updatedPokeData, null), 'utf-8')
