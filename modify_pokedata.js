ebd7a708fbe7e5484784b8e0eb27ec44875b3b2d
bwsrcurtis <bwsrcurtis@gmail.com>
2023-01-16 14:53:55 -0500

    Restructured data

--- modify_pokedata.js
+++ modify_pokedata.js
@@ -1,12 +1,22 @@
-import pokedata from './pokedata.json' assert { type: 'json' };
-// import * as fs from 'fs';
-let updatedPokeData = pokedata
+import pokeData from './pokedata.json' assert { type: 'json' };
+import * as fs from 'fs';
+let updatedPokeData = pokeData
 
 
-updatedPokeData = updatedPokeData.map((x) => {
+updatedPokeData.map((x) => {
+    // console.log(x)
+    x.name = x.pokemon_v2_pokemon.name
     x.base_experience = x.pokemon_v2_pokemon.base_experience
+    x.height = x.pokemon_v2_pokemon.height
+    x.weight = x.pokemon_v2_pokemon.weight
+    x.is_default = x.pokemon_v2_pokemon.is_default
+    x.abilities = x.pokemon_v2_pokemon.pokemon_v2_pokemonabilities
+    x.type = x.pokemon_v2_pokemon.type
+})
+updatedPokeData.forEach((e) => {
+    delete e.pokemon_v2_pokemon
 })
 
 console.log(updatedPokeData[0])
 
-// fs.writeFileSync("./pokedata.json", JSON.stringify(updatedPokeData, null), 'utf-8')
+fs.writeFileSync("./pokedata.json", JSON.stringify(updatedPokeData, null), 'utf-8')
