918d53d03a6027638d01c3b67dbff6c646f66e67
bwsrcurtis <bwsrcurtis@gmail.com>
2023-01-16 14:17:16 -0500

    Added type information for pokemon

--- modify_pokedata.js
+++ modify_pokedata.js
@@ -1,10 +1,28 @@
 import pokedata from './pokedata.json' assert { type: 'json' };
+import { pokeTypes } from './pokemonTypes.js';
+import * as fs from 'fs';
 
+let types = pokeTypes
+let updatedPokeData = pokedata
 
-console.log(pokedata[0])
+// console.log(types.pokemon_v2_pokemonsprites[0].pokemon_v2_pokemon.pokemon_v2_pokemontypes[0]['pokemon_id'])
+
+updatedPokeData.map((x) => {
+    if (x.id == types.pokemon_v2_pokemonsprites[x.id-1].pokemon_v2_pokemon.pokemon_v2_pokemontypes[0]['pokemon_id']) {
+    x.pokemon_v2_pokemon.type = [types.pokemon_v2_pokemonsprites[x.id - 1].pokemon_v2_pokemon.pokemon_v2_pokemontypes]
+    }
+})
+// console.log([types.pokemon_v2_pokemonsprites[0].pokemon_v2_pokemon.pokemon_v2_pokemontypes].length)
+
+// console.log(pokedata[0])
 // updatedPokeData.forEach(function (x) {
-//     x.imgURL = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${x.id}.png`
-//     x.pokedexDescription = descriptions[x.id-1].flavor_text.replace(/[\r\n\x0B\x0C\u0085\u2028\u2029]+/g, " ")
+//     console.log(x.id)
+//     console.log(types.pokemon_v2_pokemonsprites[x.id - 1].pokemon_v2_pokemon.pokemon_v2_pokemontypes[0]['pokemon_id'])
+//     if (x.id == types.pokemon_v2_pokemonsprites[x.id - 1].pokemon_v2_pokemon.pokemon_v2_pokemontypes[0]['pokemon_id']) {
+//         x.pokemon_v2_pokemon.type = [types.pokemon_v2_pokemonsprites[x.id - 1].pokemon_v2_pokemon.pokemon_v2_pokemontypes[0].pokemon_v2_type]
+//     }
 // })
 
-// fs.writeFileSync("./pokedata.json", JSON.stringify(updatedPokeData, null), 'utf-8')
+console.log(updatedPokeData[0])
+
+fs.writeFileSync("./pokedata.json", JSON.stringify(updatedPokeData, null), 'utf-8')
