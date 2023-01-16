4959393a05ed68365df418c397d94a68ccaef155
bwsrcurtis <bwsrcurtis@gmail.com>
2023-01-16 00:48:08 -0500

    Formatted and saved pokemon data to usable structure

---
+++ modify_pokedata.js
@@ -0,0 +1,16 @@
+
+import {pokedata} from "./pokemon_data.js";
+import { pokedexDescriptions } from "./pokedexDescriptions.js";
+import * as fs from 'fs';
+let updatedPokeData = pokedata.data.gen3_species
+let descriptions = pokedexDescriptions.data.pokemon_v2_pokemonspeciesflavortext
+
+console.log(descriptions[0])
+
+updatedPokeData.forEach(function (x) {
+    x.imgURL = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${x.id}.png`
+    x.pokedexDescription = descriptions[x.id-1].flavor_text.replace(/[\r\n\x0B\x0C\u0085\u2028\u2029]+/g, " ")
+})
+
+console.log([updatedPokeData[0], updatedPokeData[1]])
+fs.writeFileSync("./pokedata.json", JSON.stringify(updatedPokeData, null), 'utf-8')
