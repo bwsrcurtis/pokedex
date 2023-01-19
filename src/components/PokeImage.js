52053eb116ca7e50158da1a64b7fb78c38cf747a
bwsrcurtis <bwsrcurtis@gmail.com>
2023-01-19 14:46:39 -0500

    Added pokeball style background to image

--- src/components/PokeImage.js
+++ src/components/PokeImage.js
@@ -5,6 +5,7 @@ const PokeImage = ({ pokeArr, selectedPokemon, active, triggerFade }) => {
 
     return (
         <div className='image-div'>
+            <div className='image-bg'></div>
             <img onAnimationEnd={triggerFade} className={`poke-img ${active ? 'active' : 'finished'}`} src={pokeArr[selectedPokemon].imgURL} alt={pokeArr[selectedPokemon].name}></img>
         </div>
     )
